36ec999940498127acff1983b7be761baf900442
sagargueyecpe <sagar.gueye@cpe.fr>
2021-08-22 22:34:13 +0200

    V1.0

--- js/script.js
+++ js/script.js
@@ -1,6 +1,78 @@
 $(document).ready(function (){
+    // $("#carte").hide();
+    $(".backtohome").hide();
+    $("#scrollLent").click(function() {
+        $(".backtohome").show();
+        $("#carte").show();
+        $('html, body').animate({
+            scrollTop: $("#scroll").offset().top
+        }, 2000);
+        // $('html, body').animate({scrollTop: '0px'},1000);
+        setTimeout(function(){
+            $(".scroll__graphic").addClass('is-fixed');
+            $("#home").hide();
+        }, 2000);
+    });
 
-    // using d3 for convenience
+    var fixmeTop = $('.fixme').offset().top;
+    $(window).scroll(function() {
+        //baniere fix
+        var currentScroll = $(window).scrollTop();
+        if ($("#home").is(":hidden") /*&& currentScroll < fixmeTop*/) {
+            $('.fixme').css({
+                position: 'fixed',
+                top: '0',
+                left: '0'
+            });
+            $(".scroll__graphic").addClass('is-fixed');
+        } else {
+            $('.fixme').css({
+                position: 'static'
+            });
+        }
+
+        //button scroll to top
+        if ($(this).scrollTop() > 100) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+        //carte fixe
+        // if(currentScroll > $(".scroll__graphic").offset().top){
+        //     $(".scroll__graphic").addClass('is-fixed');
+        // }else{
+        //     $(".scroll__graphic").removeClass('is-fixed');
+        // }
+
+    });
+
+    //==================================================Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+    //===========================================remonter à laccueil
+    $(".backtohome").click(function (){
+        $("#home").show();
+        $(".backtohome").hide();
+        $(".scroll__graphic").removeClass('is-fixed');
+        $('.fixme').css({ position: 'static'});
+        $('html, body').animate({
+            scrollTop: 0
+        }, {
+            duration: 800,
+            complete: function () {
+                console.log("finissss");
+                $("#carte").hide();
+            }
+        });
+
+    });
+
+    //==========================================for ScrollTrigger
+
+
+    // ===============================================using d3 for convenience
     var container = d3.select("#scroll");
     var graphic = container.select(".scroll__graphic");
     var chart = graphic.select(".chart");
@@ -66,34 +138,34 @@ $(document).ready(function (){
 
 
         //MAPS SUR LES MATERIAUX DU PLAN
-        if(response.index==1 && $("map[name='RDC02']").length==0){
-            $test='<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>';
-            $('<map name="RDC02">' +
-                '                <area class="ordi demo popover" id="open"  shape="rect" coords="271,213,309,234" alt="bureau 1" href="bureau1.htm" ' +
-                '                data-toggle="popover" title="Popover title" data-content="'+$test+'">' +
-                '                <area class="ordi" shape="rect" coords="339,138,375,155" alt="bureau 2" href="bureau2.htm">' +
-                '                <area class="ordi" shape="rect" coords="367,207,409,225" alt="bureau 3" href="bureau3.htm">' +
-                '                <area class="ordi" shape="rect" coords="403,293,446,313" alt="bureau 4" href="bureau4.htm">' +
-                '                <area class="ordi" shape="rect" coords="433,197,459,241" alt="bureau 5" href="bureau5.htm">' +
-                '                <area class="ordi" shape="rect" coords="471,240,501,260" alt="bureau 6" href="bureau6.htm">' +
-                '                <area class="ordi" shape="rect" coords="648,428,665,443" alt="bureau 8" href="bureau8.htm">' +
-                '                <area class="ordi" shape="rect" coords="512,290,543,311" alt="bureau 7" href="bureau7.htm">' +
-                '</map>'
-            ).insertAfter($(".chart img") );
-            $("body").on("hover","#open",function(e) {
-                e.preventDefault();
-                //$( "map area.ordi").slideToggle("slow");
-                console.log(e);
-                $("map area.popover").tooltip({
-                    container: 'body',
-                    trigger: 'hover',
-                    title:"eeeeee",
-                    content: "And here's some amazing content. It's very engaging. Right"
-                });
-            });
+        //if(response.index==1 && $("map[name='RDC02']").length==0){
 
+            // $test='<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>';
+            // $('<map name="RDC02">' +
+            //     '                <area class="ordi demo popover" id="open"  shape="rect" coords="271,213,309,234" alt="bureau 1" href="bureau1.htm" ' +
+            //     '                data-toggle="popover" title="Popover title" data-content="'+$test+'">' +
+            //     '                <area class="ordi" shape="rect" coords="339,138,375,155" alt="bureau 2" href="bureau2.htm">' +
+            //     '                <area class="ordi" shape="rect" coords="367,207,409,225" alt="bureau 3" href="bureau3.htm">' +
+            //     '                <area class="ordi" shape="rect" coords="403,293,446,313" alt="bureau 4" href="bureau4.htm">' +
+            //     '                <area class="ordi" shape="rect" coords="433,197,459,241" alt="bureau 5" href="bureau5.htm">' +
+            //     '                <area class="ordi" shape="rect" coords="471,240,501,260" alt="bureau 6" href="bureau6.htm">' +
+            //     '                <area class="ordi" shape="rect" coords="648,428,665,443" alt="bureau 8" href="bureau8.htm">' +
+            //     '                <area class="ordi" shape="rect" coords="512,290,543,311" alt="bureau 7" href="bureau7.htm">' +
+            //     '</map>'
+            // ).insertAfter($(".chart img") );
+            // $("body").on("hover","#open",function(e) {
+            //     e.preventDefault();
+            //     //$( "map area.ordi").slideToggle("slow");
+            //     console.log(e);
+            //     $("map area.popover").tooltip({
+            //         container: 'body',
+            //         trigger: 'hover',
+            //         title:"eeeeee",
+            //         content: "And here's some amazing content. It's very engaging. Right"
+            //     });
+            // });
 
-        }
+        //}
     }
 
 
